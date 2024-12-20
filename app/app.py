a766df66f82f637447c3c50035499bc90c90c24b
NAKNAO-nnct <nakanoyuki@team-lab.com>
2024-12-20 21:30:43 +0900

    fix

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 import os
 from werkzeug.utils import secure_filename
+import uuid
 
 app = Flask(__name__)
 
@@ -22,9 +23,16 @@ def upload_file():
     if file.filename == '':
         return {'error': 'No selected file'}, 400
 
+    # ランダムなファイル名を生成
+    tmp_file_name = str(uuid.uuid4())
+
+    # ファイルの保存
+    tmp_file_path = f"/tmp/{tmp_file_name}"
+    file.save(tmp_file_path)
+
     filename = secure_filename(file.filename)
     try:
-        s3.upload_file(file, S3_BUCKET, filename)
+        s3.upload_file(tmp_file_path, S3_BUCKET, filename)
         return {'message': 'File uploaded successfully', 'filename': filename}, 200
     except (NoCredentialsError, PartialCredentialsError):
         return {'error': 'Credentials not available'}, 500
